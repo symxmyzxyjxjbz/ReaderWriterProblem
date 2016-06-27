19ce441c6b5f4f7a1beae28ea870b4aa9f6fda4e
FlorianFrank <ffrank@hm.edu>
2016-06-27 14:14:30 +0200

    Initial Commit

---
+++ ReaderWriterProblem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ReaderWriterProblem
+//
+//  Created by Florian Frank on 27.06.16.
+//  Copyright © 2016 Florian_Frank. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
